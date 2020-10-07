a2add9e294e8779ffdbf6e4a3906db83c62e0e4f
chindarcy <chindarcy@gmail.com>
2020-10-07 18:10:20 +0800

    加入Scrollview套件

---
+++ application/Phoenix.Project1/Assets/Plugins/SuperScrollView/Scripts/Common/CommonDefine.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SuperScrollView
+{
+    public enum ItemCornerEnum
+    {
+        LeftBottom = 0,
+        LeftTop,
+        RightTop,
+        RightBottom,
+    }
+
+
+    public enum ListItemArrangeType
+    {
+        TopToBottom,
+        BottomToTop,
+        LeftToRight,
+        RightToLeft,
+    }
+}
