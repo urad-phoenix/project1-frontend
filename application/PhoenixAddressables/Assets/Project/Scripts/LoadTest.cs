de2a800f209f03cf2c228d06955852b75ad9275a
chindarcy <chindarcy@gmail.com>
2020-10-12 18:31:45 +0800

    Add Addressables 測試專案

---
+++ application/PhoenixAddressables/Assets/Project/Scripts/LoadTest.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement.ResourceLocations;
+using UnityEngine.UI;
+
+public class LoadTest : MonoBehaviour
+{
+    public Button LodeByLabelBtn;
+
+    public Button LoadByGroupBtn;
+
+    public void LoadByLabel()
+    {        
+    }
+    public void LoadByGroup()
+    {
+
+    }
+}
