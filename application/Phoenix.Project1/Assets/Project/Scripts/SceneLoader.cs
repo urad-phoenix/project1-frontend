a9750450e02637dffe123db3621410fd9ded4109
jc <jiowchern@gmail.com>
2020-10-14 14:39:53 +0800

    載入場景增加百分比資訊。

--- application/Phoenix.Project1/Assets/Project/Scripts/SceneLoader.cs
+++ application/Phoenix.Project1/Assets/Project/Scripts/SceneLoader.cs
@@ -2,9 +2,9 @@
 using System;
 
 using System.Collections.Generic;
-
+using System.Security.Cryptography;
 using UniRx;
-
+using UnityEngine.ResourceManagement.AsyncOperations;
 using UnityEngine.ResourceManagement.ResourceProviders;
 
 
@@ -22,53 +22,60 @@ namespace Phoenix.Project1.Client
         }
         internal void Open(params string[] scene_names)
         {
-
-            
-            
-
-            var all = new List<IObservable<SceneInstance>>();
-            all.AddRange(_Unload());
+            var all = new List<IObservable<AsyncOperationHandle<SceneInstance>>>();
             all.AddRange(_Load(scene_names));
-            UniRx.Observable.Merge(all).Subscribe(_AddScene);
+
+            var obs = from unloadDone in UniRx.Observable.Merge(_Unload()).LastOrDefault()
+                        from handle in UniRx.Observable.Merge(all)
+                        select handle ;
+
+            obs.Subscribe( handle=> _AddScene(handle, scene_names.Length));
         }
 
-        private List<IObservable<SceneInstance>> _Load(string[] scene_names)
+        private List<IObservable<AsyncOperationHandle<SceneInstance>>> _Load(string[] scene_names)
         {
             var length = scene_names.Length;
-            var instances = new List<IObservable<SceneInstance>>();
+            var instances = new List<IObservable<AsyncOperationHandle<SceneInstance>>>();
             for (int i = 0; i < length; i++)
             {
                 var sceneName = scene_names[i];
-                var obs = from hnd in UniRx.Observable.Defer(() => UnityEngine.AddressableAssets.Addressables.LoadSceneAsync(sceneName, UnityEngine.SceneManagement.LoadSceneMode.Additive).AsObserver() )
-                          select hnd.Result;
+                var obs = from hnd in UniRx.Observable.Defer(() => UnityEngine.AddressableAssets.Addressables.LoadSceneAsync(sceneName, UnityEngine.SceneManagement.LoadSceneMode.Additive).AsHandleObserver() )
+                          select hnd;
                 instances.Add(obs)  ;
             }
             return instances;
         }
 
-        private void _AddScene(SceneInstance obj)
+        private List<IObservable<AsyncOperationHandle<SceneInstance>>> _Unload()
         {
-            if(obj.Scene.IsValid())
-                _Scenes.Push(obj);
-        }
-
-        private List<IObservable<SceneInstance>> _Unload()
-        {
-
-
             var length = _Scenes.Count;
-            var instances = new List<IObservable<SceneInstance>>();
+            var instances = new List<IObservable<AsyncOperationHandle<SceneInstance>>>();
             while (_Scenes.Count > 0)
             {
                 var instance = _Scenes.Pop();
-                var obs = from hnd in UniRx.Observable.Defer(() => UnityEngine.AddressableAssets.Addressables.UnloadSceneAsync(instance).AsObserver())
-                          select hnd.Result;
-                instances.Add(obs) ;
+                var obs = from hnd in UniRx.Observable.Defer(() => UnityEngine.AddressableAssets.Addressables.UnloadSceneAsync(instance).AsHandleObserver())
+                          select hnd;
+                instances.Add(obs);
             }
             return instances;
-
-
         }
+
+
+        private void _AddScene(AsyncOperationHandle<SceneInstance> handle,float count)
+        {
+            var percent = (handle.PercentComplete + _Scenes.Count) / count;
+            UnityEngine.Debug.Log($"scene load : {percent}% ");
+
+            if (!handle.IsDone)
+            {
+                return;
+            }
+
+            if (!handle.Result.Scene.IsValid())
+                return;
+            _Scenes.Push(handle.Result);
+        }
+
 
         internal void OpenDashboard()
         {
