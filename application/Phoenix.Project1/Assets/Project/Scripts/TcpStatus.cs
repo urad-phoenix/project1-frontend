940907e76c15ec1c767a194dea110bfb39997f22
jc <jiowchern@gmail.com>
2020-11-11 16:38:34 +0800

    [PHP-13] Scripts 植入。

--- application/Phoenix.Project1/Assets/Project/Scripts/TcpStatus.cs
+++ application/Phoenix.Project1/Assets/Project/Scripts/TcpStatus.cs
@@ -5,6 +5,7 @@ using Regulus.Utility;
 using System.Linq;
 using Regulus.Remote;
 using System;
+using Phoenix.Project1.Client.Scripts;
 
 namespace Phoenix.Project1.Client
 {
@@ -13,23 +14,27 @@ namespace Phoenix.Project1.Client
         private readonly TcpAgent _Agent;
         private readonly CommandConsole _Console;
         private readonly INotifierQueryable _Queryable;
+        private readonly IDisposable _Runner;
 
         public TcpStatus(Regulus.Remote.IProtocol protocol, Regulus.Utility.Console.IInput input, Regulus.Utility.Console.IViewer viewer)
         {
             _Agent = new Phoenix.Project1.Client.TcpAgent(protocol);
             _Console = new Phoenix.Project1.Client.CommandConsole(protocol, this, input, viewer);
             _Queryable = _Agent;
+            _Runner = new Scripts.Runner(_Agent, new Scripts.ScriptsCommander("s-{0}", _Console.Command), viewer);
         }
         INotifierQueryable IServiceStatus.Queryable => _Queryable;
         void IStatus.Enter()
         {
             _Console.Launch();
             _Console.Command.Run("create-agent", new string[0]).Single();
+
             
         }
 
         void IStatus.Leave()
         {
+            _Runner.Dispose();
             _Console.Shutdown();
         }
 
