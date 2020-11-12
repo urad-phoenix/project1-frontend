10e266cb2d04a378076dbdb019a9a1f776d0731a
chindarcy <chindarcy@gmail.com>
2020-11-12 14:19:41 +0800

    Add excel data convert tools

---
+++ application/Phoenix.Project1/Assets/Editor/TimelineTools/DataGeneratorSetting.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace Phoenix.Project1.DataConvertTool
+{
+    [CreateAssetMenu(menuName = "Urad/DataGeneratorSetting")]
+    public class DataGeneratorSetting : ScriptableObject
+    {
+        public string OutputPath = ".../.../.../.../.../.../project1-configs/";
+        public string SourcePath = "Asset/Project/Assetbundles/Timelines/";
+        public const string EXT_XSD_FILES = "*.xsd";
+        public const string EXT_XML_FILES = "*.xml";
+        public const string EXT_XLS_FILES = "*.xls";
+        public const string EXT_XLSX_FILES = "*.xlsx";
+        public const string EXT_XLSM_FILES = "*.xlsm";
+    }
+}
