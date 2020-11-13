164796593a2c6618d68ae4d1390eff188410369b
chindarcy <chindarcy@gmail.com>
2020-11-13 14:24:06 +0800

    Modify stage and role locations

--- application/Phoenix.Project1/Assets/Project/Scripts/Scene/CharacterLocator.cs
+++ application/Phoenix.Project1/Assets/Project/Scripts/Scene/CharacterLocator.cs
@@ -9,7 +9,7 @@ namespace TP.Scene.Locators
         #if UNITY_EDITOR
         [Header("Scene Editor")]
         [SerializeField]
-        public Color Color = Color.blue;
+        public Color Color = Color.blue;       
         
         [SerializeField]
         public float LabelShownPos = 0.5f;
@@ -23,6 +23,11 @@ namespace TP.Scene.Locators
         [SerializeField] 
         public float ShowSize = 0.3f;         
 
+        private void OnDrawGizmos()
+        {            
+            Gizmos.color = Color;
+            Gizmos.DrawCube(transform.position, new Vector3(ShowSize, ShowSize, ShowSize));          
+        }   
         #endif
     }
 }
